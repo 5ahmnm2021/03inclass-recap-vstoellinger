4e66c11336cdad61a1ce7c168e7f8aca3303d65b
VanessaStoellinger <vanessa.stoellinger@htl-salzburg.ac.at>
2021-03-14 12:31:45 +0100

    Added Calculator

---
+++ Assets/AddControlScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddControlScript : MonoBehaviour
+{
+
+    public InputField n1;
+    public InputField n2;
+    public Text sum;
+    public Text message;
+
+    public Color rot = Color.red;
+    public Color weiss = Color.white;
+
+    public float n1Float;
+    public float n2Float;
+
+    public void Addition()
+    {
+        try
+        {
+            n1Float = float.Parse(n1.text);
+            n1.image.color = weiss;
+            message.text = "";
+        }
+
+        catch (System.Exception)
+        {
+            message.text = "Versuch es mit Nummern!";
+            n1.image.color = rot;
+        }
+
+        try
+        {
+            n2Float = float.Parse(n2.text);
+            n2.image.color = weiss;
+        }
+
+        catch (System.Exception)
+        {
+            message.text = "Versuch es mit Nummern!";
+            n2.image.color = rot;
+        }
+
+        sum.text = (n1Float + n2Float).ToString();
+        Debug.Log("Es kommt raus: " + sum.text);
+
+    }
+}
---
+++ Assets/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("01ColorScene");
+    }
+
+    public void ChangeScene2()
+    {
+        SceneManager.LoadScene("02NumberScene");
+    }
+
+    public void ChangeScene3()
+    {
+        SceneManager.LoadScene("00WelcomeScene");
+    }
+}
